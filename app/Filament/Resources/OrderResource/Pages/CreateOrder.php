25296951015c1f3abce3a164dfb0845f582f5ce3
Kudzeri <kudzeri@gmail.com>
2025-04-01 19:37:18 +0500

    добавлена логика сохранения заказа

--- app/Filament/Resources/OrderResource/Pages/CreateOrder.php
+++ app/Filament/Resources/OrderResource/Pages/CreateOrder.php
@@ -9,4 +9,21 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateOrder extends CreateRecord
 {
     protected static string $resource = OrderResource::class;
+
+    protected function afterCreate(): void
+    {
+        $data = $this->form->getState();
+
+        $products = $data['products'] ?? [];
+
+        $syncData = [];
+
+        foreach ($products as $product) {
+            if (isset($product['product_id'], $product['quantity'])) {
+                $syncData[$product['product_id']] = ['quantity' => $product['quantity']];
+            }
+        }
+
+        $this->record->products()->sync($syncData);
+    }
 }
